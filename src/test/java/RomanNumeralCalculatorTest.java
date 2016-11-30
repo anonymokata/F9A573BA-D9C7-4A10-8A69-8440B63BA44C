adf65be3ec2e98c1559205b481a987f2db2ca08c
anonymous <anonymous>
2016-11-30 14:51:29 -0500

    Refactoring test classes to move object instantiation to setup

--- src/test/java/RomanNumeralCalculatorTest.java
+++ src/test/java/RomanNumeralCalculatorTest.java
@@ -1,19 +1,25 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RomanNumeralCalculatorTest {
 
+    private RomanNumeralCalculator calculator;
+
+    @Before
+    public void setup() {
+        calculator = new RomanNumeralCalculator();
+    }
+
     @Test
     public void adding1and1Returns2() {
-        final RomanNumeralCalculator calculator = new RomanNumeralCalculator();
         final String sum = calculator.add("I", "I");
         assertThat(sum).isEqualTo("II");
     }
 
     @Test
     public void adding2and1Returns3() {
-        final RomanNumeralCalculator calculator = new RomanNumeralCalculator();
         final String sum = calculator.add("II", "I");
         assertThat(sum).isEqualTo("III");
     }
--- src/test/java/RomanNumeralConverterTest.java
+++ src/test/java/RomanNumeralConverterTest.java
@@ -1,40 +1,43 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RomanNumeralConverterTest {
 
+    private RomanNumeralConverter converter;
+
+    @Before
+    public void setup() {
+        converter = new RomanNumeralConverter();
+    }
+
     @Test
     public void convertsIto1() {
-        final RomanNumeralConverter converter = new RomanNumeralConverter();
         final int convertedNumber = converter.convertToInt("I");
         assertThat(convertedNumber).isEqualTo(1);
     }
 
     @Test
     public void convertsIIto2() {
-        final RomanNumeralConverter converter = new RomanNumeralConverter();
         final int convertedNumber = converter.convertToInt("II");
         assertThat(convertedNumber).isEqualTo(2);
     }
 
     @Test
     public void converts1toI() {
-        final RomanNumeralConverter converter = new RomanNumeralConverter();
         final String convertedNumeral = converter.convertToRomanNumeral(1);
         assertThat(convertedNumeral).isEqualTo("I");
     }
 
     @Test
     public void converts2toII() {
-        final RomanNumeralConverter converter = new RomanNumeralConverter();
         final String convertedNumeral = converter.convertToRomanNumeral(2);
         assertThat(convertedNumeral).isEqualTo("II");
     }
 
     @Test
     public void converts3toIII() {
-        final RomanNumeralConverter converter = new RomanNumeralConverter();
         final String convertedNumeral = converter.convertToRomanNumeral(3);
         assertThat(convertedNumeral).isEqualTo("III");
     }
