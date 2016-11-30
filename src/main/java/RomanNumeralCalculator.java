12ae5d1e4df89a38a6d471c301f6934fc5d5e139
anonymous <anonymous>
2016-11-30 14:23:53 -0500

    Naive implementation of adding II and I

--- src/main/java/RomanNumeralCalculator.java
+++ src/main/java/RomanNumeralCalculator.java
@@ -1,5 +1,8 @@
 public class RomanNumeralCalculator {
     public String add(final String firstNumber, final String secondNumber) {
+        if("II".equals(firstNumber)) {
+            return "III";
+        }
         return "II";
     }
 }
--- src/test/java/RomanNumeralCalculatorTest.java
+++ src/test/java/RomanNumeralCalculatorTest.java
@@ -10,4 +10,11 @@ public class RomanNumeralCalculatorTest {
         final String sum = calculator.add("I", "I");
         assertThat(sum).isEqualTo("II");
     }
+
+    @Test
+    public void adding2and1Returns3() {
+        final RomanNumeralCalculator calculator = new RomanNumeralCalculator();
+        final String sum = calculator.add("II", "I");
+        assertThat(sum).isEqualTo("III");
+    }
 }
