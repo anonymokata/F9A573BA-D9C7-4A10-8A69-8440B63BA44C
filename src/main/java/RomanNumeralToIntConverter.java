60c9bcc20e9bc899e4d3b79e538553c51b7e4e3b
anonymous <anonymous>
2016-12-01 16:13:48 -0500

    Made checking for internally subtractive numbers like IV and IX programmatic

--- src/main/java/RomanNumeralToIntConverter.java
+++ src/main/java/RomanNumeralToIntConverter.java
@@ -11,13 +11,12 @@ public class RomanNumeralToIntConverter {
     }
 
     private Optional<Integer> getSum(final List<String> splitNumeral) {
-        try {
-            final List<Integer> integers = convertNumeralsToIntegers(splitNumeral);
-            final Integer sum = sum(integers);
-            return Optional.of(sum);
-        } catch(final IllegalArgumentException e) {
+        final List<Integer> integers = convertNumeralsToIntegers(splitNumeral);
+        final Integer sum = sum(integers);
+        if (sum == 0) {
             return Optional.empty();
         }
+        return Optional.of(sum);
     }
 
     private List<Integer> convertNumeralsToIntegers(final List<String> splitNumeral) {
@@ -25,16 +24,15 @@ public class RomanNumeralToIntConverter {
 
         for (int index = 0; index < splitNumeral.size(); index++) {
             final String currentNumeral = splitNumeral.get(index);
-            String nextNumeral = getNextNumeralIfPresent(splitNumeral, index);
+            final String nextNumeral = getNextNumeralIfPresent(splitNumeral, index);
 
-            if(nextDigitsAreFour(currentNumeral, nextNumeral)) {
-                integers.add(4);
-                index = skipNextNumeral(index);
-            } else if(nextDigitsAreNine(currentNumeral, nextNumeral)) {
-                integers.add(9);
+            if (nextNumeralIsGreaterThanCurrent(currentNumeral, nextNumeral)) {
+                final int smallerNumber = parseNumeral(currentNumeral);
+                final int largerNumber = parseNumeral(nextNumeral);
+                integers.add(largerNumber - smallerNumber);
                 index = skipNextNumeral(index);
             } else {
-                final Integer integer = convertToInteger(currentNumeral);
+                final Integer integer = parseNumeral(currentNumeral);
                 integers.add(integer);
             }
         }
@@ -42,20 +40,17 @@ public class RomanNumeralToIntConverter {
         return integers;
     }
 
+    private boolean nextNumeralIsGreaterThanCurrent(String currentNumeral, String nextNumeral) {
+        final int difference = parseNumeral(nextNumeral) - parseNumeral(currentNumeral);
+        return difference > 0;
+    }
+
     private String getNextNumeralIfPresent(final List<String> splitNumeral, final int index) {
         String nextNumeral = null;
-        if(nextNumeralExists(splitNumeral, index)) {
+        if (nextNumeralExists(splitNumeral, index)) {
             nextNumeral = splitNumeral.get(index + 1);
         }
         return nextNumeral;
-    }
-
-    private boolean nextDigitsAreFour(final String currentNumeral, final String nextNumeral) {
-        return RomanDigit.ONE.getNumeralValue().equals(currentNumeral) && RomanDigit.FIVE.getNumeralValue().equals(nextNumeral);
-    }
-
-    private boolean nextDigitsAreNine(final String currentNumeral, final String nextNumeral) {
-        return RomanDigit.ONE.getNumeralValue().equals(currentNumeral) && RomanDigit.TEN.getNumeralValue().equals(nextNumeral);
     }
 
     private int skipNextNumeral(int index) {
@@ -67,21 +62,21 @@ public class RomanNumeralToIntConverter {
         return index + 1 < splitNumeral.size();
     }
 
-    private Integer convertToInteger(final String value) {
-        if(RomanDigit.ONE.getNumeralValue().equals(value)) {
+    private Integer parseNumeral(final String value) {
+        if (RomanDigit.ONE.getNumeralValue().equals(value)) {
             return 1;
-        } else if(RomanDigit.FIVE.getNumeralValue().equals(value)){
+        } else if (RomanDigit.FIVE.getNumeralValue().equals(value)) {
             return 5;
-        } else if(RomanDigit.TEN.getNumeralValue().equals(value)){
+        } else if (RomanDigit.TEN.getNumeralValue().equals(value)) {
             return 10;
         } else {
-            throw new IllegalArgumentException();
+            return 0;
         }
     }
 
     private Integer sum(final List<Integer> integers) {
         Integer sum = 0;
-        for(Integer number : integers) {
+        for (Integer number : integers) {
             sum += number;
         }
         return sum;
