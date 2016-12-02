7acc760d4bf8431c8714bace439cabc37fb51b94
anonymous <anonymous>
2016-12-02 14:19:32 -0500

    Refactored sum logic to use blocked numeral

--- src/main/java/RomanNumeralToIntConverter.java
+++ src/main/java/RomanNumeralToIntConverter.java
@@ -10,60 +10,7 @@ public class RomanNumeralToIntConverter {
             return Optional.empty();
         }
 
-        return getSum(splitNumeral);
-    }
-
-    private boolean individualBlocksAreInvalid(final List<String> blockedNumeral) {
-        boolean isValid = true;
-        for(final String block : blockedNumeral) {
-            if(block.length() == 0) {
-                isValid = false;
-            } else if(blockContainsOnlyOneTypeOfNumeral(block)) {
-                isValid = checkIfSingleTypeValid(block);
-            } else {
-                isValid = block.length() == 2 && secondDigitGreaterThanFirst(block);
-            }
-
-            if(!isValid) {
-                break;
-            }
-        }
-        return !isValid;
-    }
-
-    private boolean secondDigitGreaterThanFirst(final String block) {
-        final String firstDigit = block.substring(0, 1);
-        final String secondDigit = block.substring(1);
-        return RomanDigit.parseNumeral(secondDigit) > RomanDigit.parseNumeral(firstDigit);
-    }
-
-    private boolean checkIfSingleTypeValid(final String block) {
-        final String firstDigit = getFirstDigit(block);
-        if(RomanDigit.parseNumeral(firstDigit) == 0) {
-            return false;
-        } else if(RomanDigit.numeralIsPowerOfTen(firstDigit)) {
-            return block.length() < 4;
-        } else {
-            return block.length() == 1;
-        }
-    }
-
-    private boolean blockContainsOnlyOneTypeOfNumeral(final String block) {
-        return block.length() == 1 || checkIfAllDigitsMatch(block);
-    }
-
-    private boolean checkIfAllDigitsMatch(final String block) {
-        final String firstDigit = getFirstDigit(block);
-        for(final String numeral : block.split("")) {
-            if(!numeral.equals(firstDigit)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private String getFirstDigit(final String block) {
-        return block.substring(0, 1);
+        return getSum(blockedNumeral);
     }
 
     private List<String> combineNumeralIntoBlocks(final List<String> splitNumeral) {
@@ -94,43 +41,94 @@ public class RomanNumeralToIntConverter {
         return parsedNumeral > parsedLastNumeral;
     }
 
-    private Optional<Integer> getSum(final List<String> splitNumeral) {
-        final List<Integer> integers = convertNumeralsToIntegers(splitNumeral);
-        final Integer sum = sum(integers);
-        if (sum == 0) {
-            return Optional.empty();
+    private boolean individualBlocksAreInvalid(final List<String> blockedNumeral) {
+        boolean isValid = true;
+        for(final String block : blockedNumeral) {
+            if(block.length() == 0) {
+                isValid = false;
+            } else if(blockContainsOnlyOneTypeOfNumeral(block)) {
+                isValid = checkIfSingleTypeValid(block);
+            } else {
+                isValid = block.length() == 2 && secondDigitGreaterThanFirst(block);
+            }
+
+            if(!isValid) {
+                break;
+            }
         }
+        return !isValid;
+    }
+
+    private boolean blockContainsOnlyOneTypeOfNumeral(final String block) {
+        return block.length() == 1 || checkIfAllDigitsMatch(block);
+    }
+
+    private boolean checkIfAllDigitsMatch(final String block) {
+        final String firstDigit = getFirstDigit(block);
+        for(final String numeral : block.split("")) {
+            if(!numeral.equals(firstDigit)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean checkIfSingleTypeValid(final String block) {
+        final String firstDigit = getFirstDigit(block);
+        if(RomanDigit.parseNumeral(firstDigit) == 0) {
+            return false;
+        } else if(RomanDigit.numeralIsPowerOfTen(firstDigit)) {
+            return block.length() < 4;
+        } else {
+            return block.length() == 1;
+        }
+
+    }
+
+    private String getFirstDigit(final String block) {
+        return block.substring(0, 1);
+    }
+
+    private boolean secondDigitGreaterThanFirst(final String block) {
+        final String firstDigit = block.substring(0, 1);
+        final String secondDigit = block.substring(1);
+        return RomanDigit.parseNumeral(secondDigit) > RomanDigit.parseNumeral(firstDigit);
+    }
+
+    private Optional<Integer> getSum(final List<String> blockedNumeral) {
+        final List<Integer> integers = convertNumeralsToIntegers(blockedNumeral);
+        final Integer sum = sum(integers);
         return Optional.of(sum);
     }
 
-    private List<Integer> convertNumeralsToIntegers(final List<String> splitNumeral) {
+    private List<Integer> convertNumeralsToIntegers(final List<String> blockedNumeral) {
         final List<Integer> integers = new ArrayList<>();
 
-        for (int index = 0; index < splitNumeral.size(); index++) {
-            final String currentNumeral = splitNumeral.get(index);
-            final String nextNumeral = getNextNumeralIfPresent(splitNumeral, index + 1);
-
-            if (nextNumeralIsGreaterThanCurrent(currentNumeral, nextNumeral)) {
-                addDifferenceOfNumeralsToList(integers, currentNumeral, nextNumeral);
-                index = skipNextNumeral(index);
-            } else {
-                final Integer integer = RomanDigit.parseNumeral(currentNumeral);
-                integers.add(integer);
-            }
+        for(final String block : blockedNumeral) {
+            final List<String> splitBlock = Arrays.asList(block.split(""));
+            final int blockSum = getBlockSum(splitBlock);
+            integers.add(blockSum);
         }
 
         return integers;
     }
 
-    private void addDifferenceOfNumeralsToList(final List<Integer> integers, final String currentNumeral, final String nextNumeral) {
-        final int smallerNumber = RomanDigit.parseNumeral(currentNumeral);
-        final int largerNumber = RomanDigit.parseNumeral(nextNumeral);
-        integers.add(largerNumber - smallerNumber);
-    }
+    private int getBlockSum(final List<String> splitBlock) {
+        int blockSum = 0;
 
-    private boolean nextNumeralIsGreaterThanCurrent(final String currentNumeral, final String nextNumeral) {
-        final int difference = RomanDigit.parseNumeral(nextNumeral) - RomanDigit.parseNumeral(currentNumeral);
-        return difference > 0;
+        for (int index = 0; index < splitBlock.size(); index++) {
+            final String currentNumeral = splitBlock.get(index);
+            final String nextNumeral = getNextNumeralIfPresent(splitBlock, index + 1);
+
+            if (nextNumeralIsGreaterThanCurrent(currentNumeral, nextNumeral)) {
+                final Integer result = subtract(nextNumeral, currentNumeral);
+                blockSum += result;
+                break;
+            } else {
+                blockSum += RomanDigit.parseNumeral(currentNumeral);
+            }
+        }
+        return blockSum;
     }
 
     private String getNextNumeralIfPresent(final List<String> splitNumeral, final int nextNumeralIndex) {
@@ -141,13 +139,19 @@ public class RomanNumeralToIntConverter {
         return nextNumeral;
     }
 
-    private int skipNextNumeral(int index) {
-        index++;
-        return index;
-    }
-
     private boolean nextNumeralExists(final List<String> splitNumeral, final int nextNumeralIndex) {
         return nextNumeralIndex < splitNumeral.size();
+    }
+
+    private boolean nextNumeralIsGreaterThanCurrent(final String currentNumeral, final String nextNumeral) {
+        final int difference = RomanDigit.parseNumeral(nextNumeral) - RomanDigit.parseNumeral(currentNumeral);
+        return difference > 0;
+    }
+
+    private Integer subtract(final String nextNumeral, final String currentNumeral) {
+        final int smallerNumber = RomanDigit.parseNumeral(currentNumeral);
+        final int largerNumber = RomanDigit.parseNumeral(nextNumeral);
+        return largerNumber - smallerNumber;
     }
 
     private Integer sum(final List<Integer> integers) {
